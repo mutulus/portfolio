44f42ed41e6f791959eb951a5f0e58491f5d65c5
Arturs Mednis <artursmednis2003@gmail.com>
2023-10-11 11:55:20 +0200

    avance

--- src/index.php
+++ src/index.php
@@ -6,8 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../style/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 
     <title>Arturs</title>
 </head>
@@ -15,11 +14,18 @@
 <?php
 include '../partials/nav.html';
 ?>
-<h1>Arturs Mednis</h1>
 
+<div class="container">
+    <h1>Arturs Mednis</h1>
+</div>
+<footer>
+   <ul>
+       <li>test</li>
+       <li>tes</li>
+       <li>test</li>
+   </ul>
+</footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
 </body>
+
 </html>
